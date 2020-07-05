8188322a9761fa0872b0bba18c0690f41fdd43f1
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-05 08:49:49 +0530

    Adding new file xyz.java

---
+++ xyz.java
@@ -0,0 +1,17 @@
+class xyz{
+		public static void main(String[] args) {
+		System.out.println("Second Day Git.");
+	}
+
+		public int add(){
+			int x = 10;
+			int y = 20;
+			return (x+y)
+		}
+
+		public int mul(){
+			int x = 10;
+			int y = 20;
+			return (x*y)
+		}
+}
